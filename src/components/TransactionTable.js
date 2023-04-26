0982e8d43ed5b8f0be5193fb03d81a8e545b1fe8
SarahMumbua <mumbuasarah2030@gmail.com>
2023-04-26 10:55:54 +0300

    added form inputs to the transactionTable component

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TransactionTable = ({ transactions }) => {
+  const [form, setForm] = useState({
+    date: '',
+    description: '',
+    category: '',
+    amount: ''
+  });
+
+  const handleFormChange = (event) => {
+    const { name, value } = event.target;
+    setForm((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+    // Call an API or add the form data to the table
+  };
+
   return (
-    <div className="table-responsive">
-      <table className="table table-sm">
+    <div className="container my-4">
+      <form onSubmit={handleSubmit}>
+        <div className="row mb-3">
+          <div className="col-sm-3">
+            <input
+              type="date"
+              className="form-control"
+              name="date"
+              value={form.date}
+              onChange={handleFormChange}
+            />
+          </div>
+          <div className="col-sm-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Description"
+              name="description"
+              value={form.description}
+              onChange={handleFormChange}
+            />
+          </div>
+          <div className="col-sm-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Category"
+              name="category"
+              value={form.category}
+              onChange={handleFormChange}
+            />
+          </div>
+          <div className="col-sm-3">
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Amount"
+              name="amount"
+              value={form.amount}
+              onChange={handleFormChange}
+            />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary mb-3">
+          Add Transaction
+        </button>
+      </form>
+      <table className="table table-bordered table-striped table-hover">
         <thead>
           <tr>
             <th>Date</th>
@@ -13,7 +77,7 @@ const TransactionTable = ({ transactions }) => {
           </tr>
         </thead>
         <tbody>
-          {transactions.map(transaction => (
+          {transactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.date}</td>
               <td>{transaction.description}</td>
